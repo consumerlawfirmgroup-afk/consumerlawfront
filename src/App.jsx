6e15b60a601cebcc14a339abc7a2310aafd8beb4
Felipe Avila <126629999+yazzuke@users.noreply.github.com>
2025-09-22 17:45:43 -0500

    Display pending payment message in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import ScrollToTop from "../src/utils/ScrollToTop";
 import "./i18n";
 
 function App() {
+
+  const hasPendingPayment = true; 
+
   return (
     <Router>
       <ScrollToTop />
-      <MainLayout>
-        <AppRoutes />
-      </MainLayout>
+      {hasPendingPayment ? (
+        <div className="bg-negro-100 min-h-screen flex items-center justify-center">
+          <h1 className="text-3xl font-bold underline text-center my-4">
+            SALDO PENDIENTE, CANCELAR LO MAS PRONTO POSIBLE PARA HABILITAR CONTENIDO
+          </h1>
+        </div>
+      ) : (
+        <MainLayout>
+          <AppRoutes />
+        </MainLayout>
+      )}
     </Router>
   );
 }
